e4af89e312c34f990fcdbf7db2a463921306696c
Krazyer <105530207+Krazyer@users.noreply.github.com>
2024-11-15 18:29:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "subscription_id" {
+  description = "The Azure Subscription ID"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "The name of the virtual machine"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "The size of the virtual machine"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "The admin username for the virtual machine"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "The admin password for the virtual machine"
+  type        = string
+}
